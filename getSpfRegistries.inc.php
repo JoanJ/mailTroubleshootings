3b9d9b51617c540ea5babdeadd55971b8a0b71a4
Joan Juventench <joan.juventench@launchmetrics.com>
2019-05-16 14:48:16 +0200

    fix spf1

--- getSpfRegistries.inc.php
+++ getSpfRegistries.inc.php
@@ -8,29 +8,32 @@ function GetSpf($entrada,$added) {
     $ResultArrayspf = dns_get_record($entrada, DNS_TXT);
     if(!$ResultArrayspf){return false;}
 
+    //iterate in DNS TXT query looking for v=spf1 entry
     foreach ($ResultArrayspf as $record) {
         if ($record['type'] == 'TXT') {
             $txt = strtolower($record['txt']);
             // An SPF record can be empty (no mechanism)
             if ($txt == 'v=spf1' || stripos($txt, 'v=spf1 ') === 0) {
                 $spfRecords[] = $txt;
-                $isEntry = strpos($txt, 'xxxxx');
+                $entryNoL = $spfRecords[0];
+                $isEntry = strpos($txt, 'launchmetrics');
                 }
             }
         }
+        //check if launchemtrics entry is in 
         if ($isEntry !== false || $added == false ){print_r($spfRecords[0]);}
         else{
             print_r($spfRecords[0]);
             print "<p></p>";
             echo " Creem SPF :  ";
-            print_r(spfString($txt));}
+            print_r(spfString($entryNoL));}
     
 
 }
 
 function spfString($stringSpf){
     
-    $spfentryL = " include:_shortspf.xxxxx.com ";
+    $spfentryL = " include:_shortspf.launchmetrics.com ";
     //afegim entrada spf de launchmetrics
     $llargada = strlen($stringSpf);
     $llargada = $llargada - 4;
